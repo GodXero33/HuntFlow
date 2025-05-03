0a9ee2832c93b3bc375c83cd03a921eab9f94a46
GodXero <godxero.dev@gmail.com>
2025-05-03 22:01:41 +0530

    Snake head movement with acurate snake movement

--- js/snake.js
+++ js/snake.js
@@ -11,20 +11,23 @@ class SnakeBodyPiece {
 export default class Snake {
 	constructor () {
 		this.head = new SnakeBodyPiece(300, 300, 20);
+
 		this.speed = 0.1;
 		this.turnSpeed = 0.002;
 		this.headingAngle = 0;
+
 		this.turnLeft = false;
 		this.turnRight = false;
-		this.oAngle = 0;
-		this.oAngleFact = Math.PI / 8;
-		this.oAngleCounter = 0;
+
+		this.wobbleAngle = 0;
+		this.wobbleAmplitude = Math.PI / 8;
+		this.wobblePhase = 0;
 	}
 
 	updateMovement (dt) {
 		if (!this.turnLeft && !this.turnRight) {
-			this.oAngle = Math.sin(this.oAngleCounter) * this.oAngleFact + this.oAngleFact;
-			this.oAngleCounter = (this.oAngleCounter + 0.1) % (Math.PI * 2);
+			this.wobbleAngle = Math.sin(this.wobblePhase) * this.wobbleAmplitude + this.wobbleAmplitude;
+			this.wobblePhase = (this.wobblePhase + 0.1) % (Math.PI * 2);
 		}
 
 		if (this.turnLeft) this.headingAngle += this.turnSpeed * dt;
@@ -32,8 +35,8 @@ export default class Snake {
 
 		this.headingAngle %= Math.PI * 2;
 
-		this.head.position.x += Math.sin(this.headingAngle + this.oAngle) * this.speed  * dt;
-		this.head.position.y += Math.cos(this.headingAngle + this.oAngle) * this.speed  * dt;
+		this.head.position.x += Math.sin(this.headingAngle + this.wobbleAngle) * this.speed  * dt;
+		this.head.position.y += Math.cos(this.headingAngle + this.wobbleAngle) * this.speed  * dt;
 	}
 
 	update (dt) {
@@ -46,7 +49,7 @@ export default class Snake {
 
 		ctx.save();
 		ctx.translate(head.position.x, head.position.y);
-		ctx.rotate(-this.headingAngle - this.oAngle);
+		ctx.rotate(-(this.headingAngle + this.wobbleAngle));
 		ctx.fillRect(-head.size * 0.5, -head.size * 0.5, head.size, head.size);
 		ctx.restore();
 	}
